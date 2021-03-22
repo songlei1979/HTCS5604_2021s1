227d048c96c1dd60d3fa30da432c9aea657d1738
Lei <gabriel_sl19798@hotmail.com>
2021-03-23 11:34:07 +1300

    correct PAYE file and testing file

--- introductionToPython/testPAYE.py
+++ introductionToPython/testPAYE.py
@@ -20,8 +20,34 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(1470.175, tax)
         tax = PAYE.lessThan48000(48000)
         self.assertEqual(7420, tax)
-        tax = PAYE.lessThan14000(48001)
+        tax = PAYE.lessThan48000(48001)
+        self.assertIs(None, tax)
+
+    def test_lessThan70000(self):
+        tax = PAYE.lessThan70000(48000)
         self.assertIsNone(None, tax)
+        tax = PAYE.lessThan70000(48001)
+        self.assertEqual(7420.3, tax)
+        tax = PAYE.lessThan70000(70000)
+        self.assertEqual(14020, tax)
+        tax = PAYE.lessThan70000(70001)
+        self.assertIs(None, tax)
+
+    def test_lessThan180000(self):
+        tax = PAYE.lessThan180000(70000)
+        self.assertIsNone(None, tax)
+        tax = PAYE.lessThan180000(70001)
+        self.assertEqual(14020.33, tax)
+        tax = PAYE.lessThan180000(180000)
+        self.assertEqual(50320, tax)
+        tax = PAYE.lessThan180000(180001)
+        self.assertIs(None, tax)
+
+    def test_over180000(self):
+        tax = PAYE.overThan180000(180000)
+        self.assertIsNone(None, tax)
+        tax = PAYE.overThan180000(180001)
+        self.assertEqual(50320.39, tax)
 
 if __name__ == '__main__':
     unittest.main()
